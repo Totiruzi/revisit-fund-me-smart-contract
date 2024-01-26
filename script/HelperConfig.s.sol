bc429a74e8a8d033ac459b9d25c8cd82f9fb99ea
mechi <totiruzi@yahoo.com>
2024-01-26 14:35:58 +0300

    Create-Test-Script-Mock-FundMe

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,75 @@
+/**
+ * @title Deploy mocks when we are on local anvil chain
+ * @author Chris 
+ * @notice Keep track of contract address across different chains
+ */
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Script } from "forge-std/Script.sol";
+import { MockV3Aggregator } from "../test/mock/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // If we are on local anvil, we deploy mocks
+    // otherwise, grab the existing address from the live network
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; //ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if(block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+ 
+    // Grab the existing address from the live network
+    function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
+        // price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns(NetworkConfig memory){
+        // price feed address
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    
+    // We are on local anvil, we deploy mocks
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory){
+        // price feed address
+        // If priceFeed does not exist that is the method has not been called yet
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed : address(mockPriceFeed)
+        });
+
+        return anvilConfig;
+    }
+}
