1fba82452cc2a4a10c0d32b9c8a515e5afdbd60a
mechi <totiruzi@yahoo.com>
2024-01-31 15:09:23 +0300

    Install-Foundary-Dev-Tools

---
+++ src/Interaction.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+// Fund intersctiom
+// Withdrawer interaction
+
+import { Script } from "forge-std/Script.sol";
+
+pragma solidity ^0.8.19;
+
+contract FundFundMe is Script {
+    
+}
+contract WithdrawFundMe is Script {}
